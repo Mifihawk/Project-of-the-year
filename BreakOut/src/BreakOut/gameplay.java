dc6680b1f8e913771cc51c68dc6214664310f175
Mifihawk <70212603+Mifihawk@users.noreply.github.com>
2021-05-15 22:01:07 -0600

    I did it

--- BreakOut/src/BreakOut/gameplay.java
+++ BreakOut/src/BreakOut/gameplay.java
@@ -1,6 +1,7 @@
 package BreakOut;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
@@ -55,6 +56,11 @@ public class gameplay extends JPanel implements KeyListener, ActionListener
 		g.fillRect(0, 0, 692, 3);
 		g.fillRect(681, 0, 3, 592);
 		
+		//scores
+		g.setColor(Color.white);
+		g.setFont(new Font("serif", Font.BOLD,25));
+		g.drawString(""+score, 590, 30);
+		
 		//the paddle
 		g.setColor(Color.green);
 		g.fillRect(playerX, 550, 100, 8);
@@ -63,6 +69,31 @@ public class gameplay extends JPanel implements KeyListener, ActionListener
 		g.setColor(Color.blue);
 		g.fillOval(ballposX, ballposY, 20, 20);
 		
+		if(totalBricks <= 0)
+		{
+			play = false;
+			ballXdir = 0;
+			ballYdir = 0;
+			g.setColor(Color.RED);
+			g.setFont(new Font("serif", Font.BOLD,30));
+			g.drawString("You WQN ! :3, Scores:", 260, 300);
+			
+			g.setFont(new Font("serif", Font.BOLD,20));
+			g.drawString("Press Enter to Restart :D", 230, 350);
+		}	
+		
+		if(ballposY > 570)
+		{
+			play = false;
+			ballXdir = 0;
+			ballYdir = 0;
+			g.setColor(Color.RED);
+			g.setFont(new Font("serif", Font.BOLD,30));
+			g.drawString("Game Over, Scores:", 190, 300);
+			
+			g.setFont(new Font("serif", Font.BOLD,20));
+			g.drawString("Press Enter to Restart :D", 230, 350);
+		}
 		
 		g.dispose();
 	}
@@ -75,7 +106,41 @@ public class gameplay extends JPanel implements KeyListener, ActionListener
 	{
 		if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8)))
 			ballYdir = -ballYdir;
-
+		
+		A: for(int i = 0; i < map.map.length; i++)
+		{
+			for(int j = 0; j<map.map[0].length; j++)
+			{
+				if (map.map[i][j] > 0)
+				{
+					int brickX = j* map.brickWidth + 80;
+					int brickY = i * map.brickHeight + 50;
+					int brickWidth = map.brickWidth;
+					int brickHeight = map.brickHeight;
+					
+					Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+					Rectangle ballRect = new Rectangle(ballposX, ballposY, 20, 20);
+					Rectangle brickRect = rect;
+					
+					if(ballRect.intersects(brickRect))
+					{
+						map.setBrickValue(0, i, j);
+						totalBricks--;
+						score += 5;
+						
+						if(ballposX + 19 <= brickRect.x || ballposX + 1 >= brickRect.x + brickRect.width)
+						{
+							ballXdir = -ballXdir;
+						} 
+						else	
+						{
+							ballYdir = -ballYdir;
+						}
+						break A;
+					}
+				}
+			}
+		}
 	
 	ballposX += ballXdir;
 	ballposY += ballYdir;
@@ -120,6 +185,24 @@ public class gameplay extends JPanel implements KeyListener, ActionListener
 			}
 		
 		}	
+		if(e.getKeyCode() == KeyEvent.VK_ENTER)
+		{
+			if(!play)
+			{
+				play = true;
+				ballposX = 120;
+				ballposY = 350;
+				ballXdir = -1;
+				ballYdir = -2;
+				playerX = 310;
+				score = 0;
+				totalBricks = 21;
+				map = new mapgenerator(3, 7);
+				
+				repaint();
+			}
+		}
+			
 	}
 	
 
--- BreakOut/src/BreakOut/mapgenerator.java
+++ BreakOut/src/BreakOut/mapgenerator.java
@@ -1,5 +1,6 @@
 package BreakOut;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
@@ -16,8 +17,7 @@ public mapgenerator(int row, int col)
 	{
 		for(int j=0; j< map[0].length; j++)
 		{
-			if(map[i][j] > 0);
-		
+		map[i][j] = 1;
 		}
 	}
 	brickWidth = 540/col;
@@ -34,8 +34,16 @@ public void draw(Graphics2D g)
 				{
 					g.setColor(Color.white);
 					g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
+					
+					g.setStroke(new BasicStroke(3));
+					g.setColor(Color.black);
+					g.drawRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
 				}
 			}
 		}
+	}
+	public void setBrickValue(int value, int row, int col)
+	{
+		map[row][col]=value;
 	}
 }
